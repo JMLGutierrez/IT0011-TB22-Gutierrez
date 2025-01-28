d50d812d000a82961df0ef7c607d2465f627aedc
JMLGutierrez <justinmarcgutz@gmail.com>
2025-01-28 10:04:38 +0800

    Create TSA1_3b.py
    program that will display the following given output. (b. use nested while statement)

---
+++ TSA1/TSA1_3b.py
@@ -0,0 +1,12 @@
+ #A program that will display the following given output. (b. use nested while statement)
+x=1 
+
+while(x<=7): 
+    y=1 
+    if x!=2 and x!=4: 
+        while(y<=x): 
+            print(x, end="") 
+            y+=1 
+    if x!=2 and x!=4: 
+        print("") 
+    x+=1 
