42c3a69f097314327ae49cfeccfd73c4fe7fe8c1
JMLGutierrez <justinmarcgutz@gmail.com>
2025-03-18 22:20:59 +0800

    Create ts5func.py
    1. Create a program that will simulate the following mathematical operations. Design a menu that will ask the user to enter the choice and provide each functions for each operations. The functions must provide the following validation on each given input. A None value must be return.
    
    [D] - Divide (the second number or denominator must not be equal to zero)
    
    [E] - Exponentiation
    
    [R] - Remainder (the second number or denominator must not be equal to zero)
    
    [F] - Summation (the two numbers are the limits and it must be the second number must be greater than the first number, if the input is 4 and 8 the sum must be 4 + 5 + 6 + 7 + 8).

---
+++ TS5/ts5func.py
@@ -0,0 +1,57 @@
+choice='Y' 
+def divide(nume,denome): 
+    if denome!=0: 
+        print("{0} divided by {1} is {2}".format(int(nume), int(denome), nume/denome)) 
+    else: 
+        print("invalid denominator! cannot divide by zero!") 
+  
+def exponentiation(base, power): 
+    print("{0} by the power of {1} is: {2}".format(base, power, base**power)) 
+
+def remainder(nume, denome):
+    print("The remainder of {0} divided by {1} is {2}".format(nume, denome, nume%denome))
+    
+def summa(start, lim):
+    sumdef=0
+    if(start<lim):
+        for x in range(start,lim+1):
+            sumdef=sumdef+x
+            if(x!=lim):
+                print("{0} + ".format(x), end="")
+            else:
+                print("{0}".format(x))
+        print("The summation of {0} to {1} is {2}".format(start, lim, sumdef))
+    else:
+        print("Invalid! 1st number MUST BE GREATER THAN 2nd number")
+    
+    
+print("[[[[SPECIAL CALCULATOR]]]]")
+while choice=='Y': 
+    print("\n[D] - Divide") 
+    print("[E] - Exponentiation") 
+    print("[R] - Remainder") 
+    print("[F] - Summation") 
+    menu_in=input("Input operation: ") 
+    menu_in=menu_in.upper()
+    match menu_in: 
+        case 'D':
+            print("\nDIVISION MODE")
+            inpt1=float(input("Enter 1st num: ")) 
+            inpt2=float(input("Enter 2nd num: ")) 
+            divide(inpt1, inpt2) 
+        case 'E':
+            print("\nEXPONENTIATION MODE")
+            inpt1=int(input("Enter Base num: ")) 
+            inpt2=int(input("Enter By the pow: ")) 
+            exponentiation(inpt1, inpt2) 
+        case 'R':
+            print("\nREMAINDER MODE")
+            inpt1=int(input("1st num: ")) 
+            inpt2=int(input("2nd num: ")) 
+            remainder(inpt1, inpt2) 
+        case 'F': 
+            print("\nSUMMATION MODE")
+            inpt1=int(input("Enter 1st num: ")) 
+            inpt2=int(input("Enter 2nd num: ")) 
+            summa(inpt1, inpt2) 
+ 
